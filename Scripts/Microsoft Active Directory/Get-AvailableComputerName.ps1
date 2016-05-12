1fb3a20b415481717ebee9c82c811568e838c096
4D5A <4D5A@users.noreply.github.com>
2016-05-12 10:59:32 -0400

    Removed unnecessary code

---
+++ Scripts/Microsoft Active Directory/Get-AvailableComputerName.ps1
@@ -0,0 +1,19 @@
+﻿import-module ActiveDirectory
+$counter = 0
+$nameselected = "no"
+$name = "image"
+$computername = "$name$counter"
+
+Do { 
+    Write-Host "Checking for existance of $computername."
+    if (Get-ADComputer -Identity $computername) {
+    Write-Host "Count is $counter."
+    $global:counter++
+    $computername = "$name$counter"
+    Write-Host "New count is $counter."
+    }
+    }
+    Until ( ( -not $( Get-ADComputer -Identity $computername)) )
+        $nameslected = "yes"
+    Write-Host "The name will be $computername."
+    Write-Host "Replace this line with the with a function using the choosen computer name."
