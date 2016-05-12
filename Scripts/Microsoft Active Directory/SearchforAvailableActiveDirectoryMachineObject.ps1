f57e103c175bc215cdca4b933c86b33b83f2bd29
4D5A <4D5A@users.noreply.github.com>
2016-05-12 10:46:50 -0400

    Organizing Repository by File Type

---
+++ Scripts/Microsoft Active Directory/SearchforAvailableActiveDirectoryMachineObject.ps1
@@ -0,0 +1,19 @@
+﻿import-module ActiveDirectory
+$counter = 0
+$nameselected = "no"
+$server = "image$counter"
+$ErrorActionPreference = 'SilentlyContinue'
+
+Do { 
+    Write-Host "Checking for existance of $server."
+    if (Get-ADComputer -Identity $server) {
+    Write-Host "Count is $counter."
+    $global:counter++
+    $server = "image$counter"
+    Write-Host "New count is $counter."
+    }
+    }
+    Until ( ( -not $( Get-ADComputer -Identity $server)) )
+        $nameslected = "yes"
+    Write-Host "The name will be $server."
+    Write-Host "Replace this line with the with your function for chaning DNS or joining the domain."
---
+++ Scripts/Microsoft Exchange/EmailDistributionListReport.ps1
@@ -0,0 +1,8 @@
+﻿Add-Type -Path "C:\Program Files\Microsoft\Exchange\Web Services\2.0\Microsoft.Exchange.WebServices.dll"
+add-pssnapin Microsoft.Exchange.Management.PowerShell.E2010
+Get-DistributionGroup | Select-Object GroupType, Alias, OrganizationalUnit, PrimarySmtpAddress, WindowsEmailAddress | Export-Csv -NoTypeInformation  \\server\share\DistributionListReport.csv
+$ImportCSV = import-csv "\\server\share\DistributionListReport.csv"
+foreach ($line in $ImportCSV) {
+    $Alias = $line.Alias
+    Get-DistributionGroupMember $Alias | Select-Object Name | Export-Csv -NoTypeInformation \\server\share\membership_$Alias.csv
+}
